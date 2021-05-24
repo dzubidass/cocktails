8eb8fa49699f8607bc42b048d5b74c9cfcc9b002
dzubidass <kaszas.michal@tutanota.com>
2021-05-24 11:28:42 +0200

    change app name

--- app.py
+++ app.py
@@ -5,29 +5,29 @@ from flask import url_for
 from flask import request
 from flask import redirect
 
-cocktails=Flask(__name__)
+app=Flask(__name__)
 
-@cocktails.route("/")
+@app.route("/")
 def index():
 	return render_template("index.html")
 
-@cocktails.route("/mahalo/")
+@app.route("/mahalo/")
 def mahalo_page():
 	return render_template("mahalo.html")
 
-@cocktails.route("/crystal/")
+@app.route("/crystal/")
 def crystal_page():
 	return render_template("crystal.html")
 
-@cocktails.route("/siren/")
+@app.route("/siren/")
 def siren_page():
 	return render_template("siren.html")
 
-@cocktails.route("/umpalumpa/")
+@app.route("/umpalumpa/")
 def umpalumpa_page():
 	return render_template("umpalumpa.html")
 
-@cocktails.route("/la_liberte/")
+@app.route("/la_liberte/")
 def la_liberte_page():
 	return render_template("la_liberte.html")
 
@@ -35,5 +35,5 @@ def la_liberte_page():
 if __name__=="__main__":
 	debug=True
 	host="0.0.0.0"
-	cocktails.run(host,debug=debug)
+	app.run(host,debug=debug)
 
